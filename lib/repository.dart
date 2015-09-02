9e88b22671346e7916939b246e34ee66ef54f960
Don <don.j.olmstead@gmail.com>
2015-09-01 22:01:30 -0700

    Adding repository implementation from pub_api example

---
+++ lib/repository.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2015, the Speakeasy Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+library speakeasy.repository;
+
+//---------------------------------------------------------------------
+// Exports
+//---------------------------------------------------------------------
+
+export 'src/repository/copy_and_write_repository.dart';
+export 'src/repository/file_repository.dart';
+export 'src/repository/http_proxy_repository.dart';
